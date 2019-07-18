f3fc3ab24b7e275b137f2c5ccc6ec7591c3049e0
grains36 <37808854+grains36@users.noreply.github.com>
2019-07-18 15:51:46 -0400

    Add files via upload

--- report_push.php
+++ report_push.php
@@ -14,77 +14,117 @@ require_once ExternalModules::getProjectHeaderPath();
 $pushvars = new DynamicReportPushExternalModule();
 
 $settings = ExternalModules::getProjectSettingsAsArray($pushvars->PREFIX, $_GET['pid']);
+$reports = $settings['report_id']['value'];
+$reportCount = count($reports);
+//print_r ($reportCount);
 
+$ProjectID = $settings['project-id']['value'];
+if (!is_array($ProjectID)) {
+	$ProjectID = array($ProjectID);
+}
+// create an array with values of each instance of a dynamic report
+for($i=0; $i <$reportCount; ++$i) {
+
+$ProjectID = $settings['project-id']['value'];
 $ReportID = $settings['report_id']['value'];
-
 $curtoken = $settings['token1']['value'];
-
 $pushtoken = $settings['token2']['value'];
+$eventname = $settings['event_name']['value'];
+$longtoclassic = $settings['long_to_classic']['value'];
+$repeattoclassic = $settings['repeat_to_classic']['value'];
 
 
-$data = array(
-    'token' => $curtoken,
-    'content' => 'report',
-    'format' => 'json',
-    'report_id' => $ReportID,
-    'rawOrLabel' => 'raw',
-    'rawOrLabelHeaders' => 'raw',
-    'exportCheckboxLabel' => 'false',
-    'returnFormat' => 'json'
-);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, APP_PATH_WEBROOT_FULL.'api/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_VERBOSE, 0);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+			$data = array(
+			    'token' => $curtoken[$i],
+			    'content' => 'report',
+			    'format' => 'json',
+			    'report_id' => $ReportID[$i],
+			    'redcap_event_name' => $eventname[$i],
+			    'rawOrLabel' => 'raw',
+			    'rawOrLabelHeaders' => 'raw',
+			    'exportCheckboxLabel' => 'false',
+			    'returnFormat' => 'json'
+			);
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, APP_PATH_WEBROOT_FULL.'api/');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_VERBOSE, 0);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
 
-$result = curl_exec($ch);
+			//print_r ($data);
+			$result = curl_exec($ch);
 
-curl_close($ch);
+			curl_close($ch);
 
-
-$data = array(
-    'token' => $pushtoken,
-    'content' => 'record',
-    'format' => 'json',
-    'type' => 'flat',
-    'overwriteBehavior' => 'normal',
-    'forceAutoNumber' => 'false',
-    'data' => $result,
-    'returnContent' => 'count',
-    'returnFormat' => 'json'
-);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, APP_PATH_WEBROOT_FULL.'api/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_VERBOSE, 0);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
-
-$output = curl_exec($ch);
-
-$message = str_replace("}", "", $output);
-
-$message = substr ($message, 8);
-
-$message = 'Current Number of Records in Linked Project is ' . $message ;
+			$shark = json_decode($result, true);  //convert json file to a php array
 
 
-print $message;
+            // add event name to data file or destroy the event/repeating instrument fields if necessary
+            foreach ($shark as &$record) {
+            if (isset($eventname[$i])) {
+			 $record ['redcap_event_name'] = $eventname[$i];
+			}
+			if ($longtoclassic[$i] == 1) {
+				unset($record[redcap_event_name]);
+			}
 
-curl_close($ch);
+			if ($repeattoclassic[$i] == 1) {
+			    unset($record[redcap_repeat_instrument]);
+			    unset($record[redcap_repeat_instance]);
+			}
+			}
 
+			$result2 = json_encode($shark, JSON_PRETTY_PRINT); //convert data to json
 
+            //print_r ($result2);
 
+			$data = array(
+			    'token' => $pushtoken[$i],
+			    'content' => 'record',
+			    'format' => 'json',
+			    'type' => 'flat',
+			    'redcap_event_name' => $eventname[$i],
+			    'overwriteBehavior' => 'normal',
+			    'forceAutoNumber' => 'false',
+			    'data' => $result2,
+			    'returnContent' => 'count',
+			    'returnFormat' => 'json'
+			);
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, APP_PATH_WEBROOT_FULL.'api/');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_VERBOSE, 0);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+
+			$output = curl_exec($ch);
+
+			$message = str_replace("}", "", $output);
+
+			$message = substr ($message, 8);
+
+			$message = ' Current Number of Records in Project ' .$ProjectID[$i]. ' is ' . $message ;
+
+			print $message;
+			echo "<br>";
+
+			curl_close($ch);
+
+			$result = null;
+			$shark = null;
+			$result2 = null;
+            $data = null;
+
+}
